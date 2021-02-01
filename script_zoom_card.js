d635a36f604587878dcb4780345619eb33a94509
fberato <flavioberato@gmail.com>
2021-02-01 15:29:48 -0300

    ok!

--- script_zoom_card.js
+++ script_zoom_card.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	
-	$('.card').delay(1800).queue(function(next) {
+	$('.cart').delay(1800).queue(function(next) {
 		$(this).removeClass('hover');
 		$('a.hover').removeClass('hover');
 		next();
